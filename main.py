669ae65abae95374dbf30de538bd2ab1dd4492a2
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-05 21:36:56 +0200

    Create TTS job

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import os, requests, json, time
 import PyPDF2
-import requests
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Extract text from pdf
 with open('Lorem_ipsum.pdf', 'rb') as pdffile:
     pdfReader = PyPDF2.PdfReader(pdffile)
-    print(pdfReader.pages[0].extract_text())
+    pdf_text = pdfReader.pages[0].extract_text()
+
+
+# Create Text-to-Speech job
+url = "https://large-text-to-speech.p.rapidapi.com/tts"
+
+payload = {"text": pdf_text}
+headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": os.getenv('X-RapidAPI-Key'),
+    "X-RapidAPI-Host": "large-text-to-speech.p.rapidapi.com"
+}
+
+response = requests.request("POST", url, json=payload, headers=headers)
+
+print(response.text)
